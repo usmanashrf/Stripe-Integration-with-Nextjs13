288776a8065a4b718b1f6fbac7297b1ac3601203
usmanashrf <45431758+usmanashrf@users.noreply.github.com>
2023-06-06 02:01:53 +0500

    added webhook endpoint

--- stripe-integration/src/app/api/webhook/route.ts
+++ stripe-integration/src/app/api/webhook/route.ts
@@ -1,30 +1,44 @@
 import { NextResponse } from "next/server";
 import Stripe from "stripe";
 import { buffer } from "micro";
+import getRawBody from "raw-body";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
   apiVersion: "2022-11-15",
 });
-const webhookSecret = process.env.STRIPE_WEBHOOK_ENDPOINT_SECRET as string;
+const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET as string;
 
 export  async function POST(req: any, res: any){
-    const buf = await buffer(req);
-    const sig = req.headers["stripe-signature"];
-
-    let stripeEvent;
+    
     try {
-        stripeEvent = stripe.webhooks.constructEvent(buf, sig, webhookSecret);
-        console.log( 'stripeEvent', stripeEvent );
-    } catch (err: any) {
-        res.status(400).send(`Webhook Error: ${err.message}`);
-        return;
-    }
-    if ( 'checkout.session.completed' === stripeEvent.type ) {
-        const session = stripeEvent.data.object;
-        console.log( 'payment success', session );
-        res.json({ received: true });
+    const rawBody = await req.text();
+    console.log("rawBody.-----------...........----------......------",rawBody);
+    const sig = req.headers.get("stripe-signature") as string
+    //req.headers["stripe-signature"];
+    console.log("sig-----------=============-------====-=-=-", sig);
+
+    const event = stripe.webhooks.constructEvent(rawBody,sig,webhookSecret);
+
+    console.log("Hello usman-----------------");
+    console.log(event);
+
+    if ( 'checkout.session.completed' === event.type ) {
+        const session = event.data.object;
+
+        console.log( 'payment success-----------------------', session );
+
+        const line_Items  = await stripe.checkout.sessions.listLineItems(event.data.object!.id);
+console.log("Line Items==========================",line_Items);
+
+        // res.json({ received: true });
     } else {
         res.setHeader("Allow", "POST");
-        res.status(405).end("Method Not Allowed");
+        // res.status(405).end("Method Not Allowed");
     }
+    } catch (err: any) {
+        console.log("Error in webhook----------", err);
+        // res.status(400).send(`Webhook Error: ${err.message}`);
+        return;
+    }
+   
 }
